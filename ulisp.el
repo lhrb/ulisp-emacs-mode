7b3f036715672c956bd09375447bc4b2ebbfd0e8
lukas <lukas.block@pm.me>
2023-03-08 00:03:34 +0100

    add keymap & proto font lock

--- ulisp.el
+++ ulisp.el
@@ -15,7 +15,6 @@
 ;;
 ;;; Code:
 
-
 (require 'inf-lisp)
 
 (defvar ulisp-port-name "/dev/ttyACM0")
@@ -40,6 +39,7 @@ SPEED: baudrate default 9600"
    :noquery t)
   (pop-to-buffer "*ulisp*"))
 
+
 (defun ulisp-eval-sexp ()
   "Send the s-expression before the cursor to the repl."
   (interactive)
@@ -59,6 +59,68 @@ SPEED: baudrate default 9600"
       (message "No sexp found before the cursor."))))
 
 (local-set-key (kbd "C-c C-e") 'ulisp-send-lisp-form-to-serial-process)
+
+(defvar ulisp-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-e") 'ulisp-eval-sexp)
+    map))
+
+(defvar ulisp-font-lock-keywords
+  `(
+    (,(concat "\\("
+              (regexp-opt '("nil"
+                            "t"
+                            "and"
+                            "or"
+                            "not"
+                            "if"
+                            "when"
+                            "unless"
+                            "cond"
+                            "case"
+                            "let"
+                            "let*"
+                            "letrec"
+                            "setq"
+                            "defvar"
+                            "defconst"
+                            "defun"
+                            "lambda"
+                            "destructuring-bind"
+                            "progn"
+                            "prog1"
+                            "prog2"
+                            "do"
+                            "do*"
+                            "do-all-symbols"
+                            "do-external-symbols"
+                            "do-symbols"
+                            "dotimes"
+                            "dolist"
+                            "block"
+                            "return"
+                            "return-from"
+                            "tagbody"
+                            "go"
+                            "catch"
+                            "throw"
+                            "unwind-protect"
+                            "with-output-to-string"
+                            "with-input-from-string"
+                            "with-open-file"
+                            "with-slots"
+                            "with-accessors"))
+              "\\)\\>")
+     (1 font-lock-keyword-face)))
+  "Syntax highlighting for uLisp code.")
+
+(define-derived-mode ulisp-mode lisp-mode "uLisp Mode"
+  "Major mode for editing ulisp."
+  (setq-local font-lock-defaults '(ulisp-font-lock-keywords))
+  (use-local-map ulisp-keymap))
+
+(add-to-list 'auto-mode-alist '("\\.ulisp" . ulisp-mode))
+(add-hook 'ulisp-mode-hook 'paredit-mode)
 
 (provide 'ulisp)
 ;;; ulisp.el ends here
